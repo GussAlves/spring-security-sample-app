fa5dca077e375751a3376ae7b4c36ee43e33dfb7
gusal <gus.alves.araujo@gmail.com>
2021-12-14 20:54:56 -0300

    Refaturing class controller/RoutesController

--- src/main/java/com/gussalves/spring_auth/controller/RoutesController.java
+++ src/main/java/com/gussalves/spring_auth/controller/RoutesController.java
@@ -1,17 +1,46 @@
 package com.gussalves.spring_auth.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class RoutesController implements WebMvcConfigurer {
+@EnableWebSecurity // Necessary includes this annotation for class
+public class RoutesController extends WebSecurityConfigurerAdapter {
 
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/home").setViewName("home");
-		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/hello").setViewName("hello");
-		registry.addViewController("/login").setViewName("login");
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+				.authorizeRequests()
+					.antMatchers("/", "/home").permitAll() // All users have access this pages
+					.anyRequest().authenticated()
+					.and()
+				.formLogin()
+					.loginPage("/login")
+					.permitAll()
+					.and()
+				.logout()
+					.permitAll();
 	}
 
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+				User.withDefaultPasswordEncoder()
+						.username("user")
+						.password("password")
+						.roles("USER")
+						.build();
+		return new InMemoryUserDetailsManager(user);
+	}
 }
