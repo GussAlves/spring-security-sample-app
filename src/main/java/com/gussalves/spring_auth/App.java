c92d86f4b734a6f229ba19a73482486275cdc8fc
gusal <gus.alves.araujo@gmail.com>
2021-12-14 20:22:03 -0300

    Refaturing code source

--- src/main/java/com/gussalves/spring_auth/App.java
+++ src/main/java/com/gussalves/spring_auth/App.java
@@ -1,13 +1,13 @@
-package com.example.securingweb;
+package com.gussalves.spring_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecuringWebApplication {
+public class App {
 
 	public static void main(String[] args) throws Throwable {
-		SpringApplication.run(SecuringWebApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
--- src/main/java/com/gussalves/spring_auth/controller/RoutesController.java
+++ src/main/java/com/gussalves/spring_auth/controller/RoutesController.java
@@ -1,11 +1,11 @@
-package com.example.securingweb;
+package com.gussalves.spring_auth.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class RoutesController implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
