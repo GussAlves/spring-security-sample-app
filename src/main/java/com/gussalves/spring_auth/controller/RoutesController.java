0bc432552590a0649c77bcc01544fbcc6c77176c
gusal <gus.alves.araujo@gmail.com>
2021-12-14 21:52:46 -0300

    Test's ok

--- src/main/java/com/gussalves/spring_auth/controller/RoutesController.java
+++ src/main/java/com/gussalves/spring_auth/controller/RoutesController.java
@@ -1,46 +1,17 @@
 package com.gussalves.spring_auth.controller;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity // Necessary includes this annotation for class
-public class RoutesController extends WebSecurityConfigurerAdapter {
+public class RoutesController implements WebMvcConfigurer {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void addViewControllers(ViewControllerRegistry registry) {
 
-		http
-				.authorizeRequests()
-					.antMatchers("/", "/home").permitAll() // All users have access this pages
-					.anyRequest().authenticated()
-					.and()
-				.formLogin()
-					.loginPage("/login")
-					.permitAll()
-					.and()
-				.logout()
-					.permitAll();
-	}
-
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-				User.withDefaultPasswordEncoder()
-						.username("user")
-						.password("password")
-						.roles("USER")
-						.build();
-		return new InMemoryUserDetailsManager(user);
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
 	}
 }
